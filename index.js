b629639eff312e69278cda20ef1ab48f74a8773b
saulrega <p4n73ra@gmail.com>
2022-01-08 19:45:27 -0600

    fin modulo 2, metodos, estatus

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-// traemos a express
 const express = require("express");
-const { restart } = require("nodemon");
+const routerApi = require('./routes');
 
-// creamos una aplicación
 const app = express();
-
-//le decimos el puerto en que queremos que corra la aplicación
 const port = 3000;
 
-//definimos la ruta
-// tiene un callback que va a ejecutar la respuesta que enviemos al cliente.
-//el callback siempre tiene dos parámetros "req" y "res".
+app.use(express.json())
+
 app.get ("/", (req, res) => {
   res.send("Hola mi server en express");
 });
@@ -20,40 +15,15 @@ app.get ("/new-route", (req, res) => {
   res.send("Hola, soy una nueva ruta");
 });
 
-app.get ("/products", (req, res) => {
-  res.json([
-    {
-    name: "Product 1",
-    price: 1000,
-    },
-    {
-      name: "Product 2",
-      price: 2000,
-    }
-  ]);
-});
 
 
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params
-  res.json({
-    id,
-    name: "Product 2",
-    price: 2000,
-  })
-})
+
+routerApi(app)
 
 
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params
-  res.json({
-    categoryId,
-    productId,
-  })
-})
 
-//le decimos a la aplicación en que puesto escuchar
-// además creamos un callback que nos avisará cuando esté corriendo
+
+
 app.listen(port, () => {
   console.log("mi port" + port);
 });
---
+++ routes/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const productsRouter = require('./products.router');
+const userRouter = require('./users.router')
+const categoryRouter = require('./categories.router')
+
+
+function routerApi(app) {
+  const router = express.Router()
+  app.use('/api/v1', router)
+  router.use('/products', productsRouter)
+  router.use('/users', userRouter)
+  router.use('./categories', categoryRouter)
+}
+
+
+
+
+
+
+
+module.exports = routerApi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
