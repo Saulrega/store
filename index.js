b5ac445415a9adccd9cd734a75b6186438a87c5b
saulrega <p4n73ra@gmail.com>
2022-01-09 09:21:03 -0600

    primer solucion conexion api, con cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const routerApi = require('./routes');
+const cors = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
 
 const app = express();
 const port = 3000;
 
-app.use(express.json())
+app.use(express.json(), cors())
 
 app.get ("/", (req, res) => {
   res.send("Hola mi server en express");
