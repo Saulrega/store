2708e228ea4ce5c5e344a5f342ffa1cb3ebe24de
saulrega <p4n73ra@gmail.com>
2022-01-09 09:44:55 -0600

    terminando la seccion de productos

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/err
 const app = express();
 const port = 3000;
 
-app.use(express.json(), cors())
+app.use(express.json())
+const whiteList = ["http://localhost:5050"]
+const options = {
+  origin: (origin, callback) => {
+    if (whiteList.includes(origin)) {
+      callback(null, true)
+    } else{
+      callback(new Error('Acceso denegado'))
+    }
+  }
+}
+app.use(cors(options))
 
 app.get ("/", (req, res) => {
   res.send("Hola mi server en express");
